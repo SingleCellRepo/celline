5cc379d180d357570c2ff7884dd7e144fe5f70cb
YUYA556223 <yu.pisces.556223@gmail.com>
2022-09-21 18:15:50 +0900

    evoked test

--- tools/install.sh
+++ tools/install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+realpath() {
+    f=$@
+    if [ -d "$f" ]; then
+        base=""
+        dir="$f"
+    else
+        base="/$(basename "$f")"
+        dir=$(dirname "$f")
+    fi
+    dir=$(cd "$dir" && /bin/pwd)
+    echo "$dir$base"
+}
 
 ## Target exec directory
 read -e -p "Installation target directory? " install_dir
@@ -6,17 +18,24 @@ if [ -z "$install_dir" ]; then
     echo "Cannot install to empty directory"
     exit 1
 fi
-install_dir=$(cd "$(dirname "$install_dir")" && pwd)/$(basename "$install_dir")
+install_dir=$(realpath "$install_dir")
 if ! [[ -d $install_dir ]]; then
     echo "Could not find directory: $install_dir"
     exit 1
 fi
 
-install_dir="${install_dir}/celline"
-rm -rf "${install_dir}"
+echo "Installing..."
 
-declare -a arr=("test1" "test2" "test3")
-bash ./multi_selection.sh "${arr[*]}"
-result=$(cat "./sel_res.txt")
-rm "./sel_res.txt"
-echo "${result[@]}"
+rm -rf "${install_dir}/celline"
+source_loc=$(dirname "$(
+    cd "$(dirname "$0")" || exit
+    pwd
+)")
+# cd "${source_loc}" || exit
+rsync -ah "${source_loc}" "${install_dir}" --exclude '__pycache__/' --exclude '.git/' --exclude 'test/' --exclude '.vscode/' --exclude '*.ipynb' --exclude '.mypy_cache/' --exclude 'README.md' --exclude 'tools/'
+
+# declare -a arr=("test1" "test2" "test3")
+# bash ./multi_selection.sh "${arr[*]}"
+# result=$(cat "./sel_res.txt")
+# rm "./sel_res.txt"
+# echo "${result[@]}"
