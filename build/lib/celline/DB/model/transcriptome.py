7f19c1b76186845a0c719428d89195fdd13417b8
Yuya Sato <yuyasato@YuyanoMacBook-Air.local>
2023-08-14 23:31:47 +0900

    AUTOMATICALLY UPDATE

--- build/lib/celline/DB/model/transcriptome.py
+++ build/lib/celline/DB/model/transcriptome.py
@@ -1,22 +1,24 @@
 import os
 from typing import NamedTuple, Type, Optional
-from celline.DB.dev.model import BaseModel, Primary
+from celline.DB.dev.model import BaseModel, Primary, BaseSchema
 from pprint import pprint
 
+from dataclasses import dataclass
+
+@dataclass
+class Transcriptome_Schema(BaseSchema):
+    built_path: str
 
 class Transcriptome(BaseModel):
-    class Schema(NamedTuple):
-        species: Primary[str]
-        built_path: str
 
     def set_class_name(self) -> str:
         return __class__.__name__
 
-    def def_schema(self) -> Type[Schema]:
-        return Transcriptome.Schema
+    def def_schema(self) -> Type[Transcriptome_Schema]:
+        return Transcriptome_Schema
 
     def search(self, acceptable_id: str, force_search=False) -> Optional[str]:
-        target = self.get(Transcriptome.Schema, lambda d: d.species == acceptable_id)
+        target = self.get(Transcriptome_Schema, lambda d: d.species == acceptable_id)
         if len(target) > 0:
             return target[0].built_path
         return None
@@ -29,4 +31,4 @@ class Transcriptome(BaseModel):
         if (self.search(species) is not None) and (not force_update):
             print(f"Transcriptome of {species} is already exists.")
             return
-        obj.add_schema(Transcriptome.Schema(species=species, built_path=built_path))
+        obj.add_schema(Transcriptome_Schema(key=species, parent=None, children=None, title=species, built_path=built_path))
